5edd730f4b3d56a895268f4248b32ca83c734a47
Blake H <blake@indusblue.com>
2011-06-01 11:41:20 -0400

    config

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "assets/css"
+sass_dir = "assets-src/sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/js"
 
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :compact
