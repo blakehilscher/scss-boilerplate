bc18c6187cf95653efafaa17e5809455364e9b10
Blake Hilscher <blake@hilscher.ca>
2011-10-24 10:34:36 -0400

    config added

---
+++ config/compass.rb
@@ -0,0 +1,13 @@
+project_type = :rails
+http_path    = '/'
+css_dir      = 'public/css'
+sass_dir     = 'app/stylesheets'
+
+# output_style = :expanded or :nested or :compact or :compressed
+#output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions.
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
